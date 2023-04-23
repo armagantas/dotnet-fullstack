234616abc3fe8fb3019a7494132843f05152c99a
armagantas <65099385+armagantas@users.noreply.github.com>
2023-04-24 02:18:07 +0300

    Commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,33 @@
-import React from "react";
-import logo from "./logo.svg";
+import { useState } from "react";
 
 function App() {
+  const [products, setProducts] = useState([
+    { name: "Product1", price: 100.0 },
+    { name: "Product2", price: 200.0 },
+    { name: "Product3", price: 300.0 },
+  ]);
+
+  function addProduct() {
+    setProducts((prevState) => [
+      ...prevState,
+      {
+        name: "Product" + (prevState.length + 1),
+        price: prevState.length * 100 + 100,
+      },
+    ]);
+  }
+
   return (
-    <div className="App">
+    <div>
       <h1>StoreApp</h1>
+      <ul>
+        {products.map((item, index) => (
+          <li key={index}>
+            {item.name} - {item.price}
+          </li>
+        ))}
+      </ul>
+      <button onClick={addProduct}>Add Product</button>
     </div>
   );
 }
